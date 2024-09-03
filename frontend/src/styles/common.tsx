63d3ffa36a920432878845c752bf783088cbd50d
Danilo Krasic <danilokrasic@hotmail.com>
2024-09-03 09:50:53 +0200

    moved form styles to common

--- frontend/src/styles/common.tsx
+++ frontend/src/styles/common.tsx
@@ -29,13 +29,18 @@ export const Button = styled.button`
 
 export const ErrorMessage = styled.p`
   margin: 0;
-  color: ${props => props.theme.colors.salmon};
+  color: ${props => props.theme.colors.deepOrange};
 `
 
 export const NestedFormWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 20px;
+  padding: 20px 0 20px 20px;
+`
+
+export const NestedFormTitle = styled.p`
+  font-size: ${props => props.theme.fontSize.medium};
+  color: ${props => props.theme.colors.orangeYellow};
 `
 
 export const FormList = styled.ul`
